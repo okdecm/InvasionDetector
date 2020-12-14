05040edc68db16afa061ff000c81599f2e522ac0
Dec Murphy <okdecm@gmail.com>
2020-12-14 00:53:47 +0000

    Added Maps data

---
+++ Data/Maps.lua
@@ -0,0 +1,178 @@
+Maps = {
+	["0"] = {
+		["Name"] = "Eastern Kingdoms",
+		["InstanceType"] = 0
+	},
+	["1"] = {
+		["Name"] = "Kalimdor",
+		["InstanceType"] = 0
+	},
+	["13"] = {
+		["Name"] = "Testing",
+		["InstanceType"] = 0
+	},
+	["25"] = {
+		["Name"] = "Scott Test",
+		["InstanceType"] = 0
+	},
+	["29"] = {
+		["Name"] = "CashTest",
+		["InstanceType"] = 1
+	},
+	["30"] = {
+		["Name"] = "Alterac Valley",
+		["InstanceType"] = 3
+	},
+	["33"] = {
+		["Name"] = "Shadowfang Keep",
+		["InstanceType"] = 1
+	},
+	["34"] = {
+		["Name"] = "Stormwind Stockade",
+		["InstanceType"] = 1
+	},
+	["35"] = {
+		["Name"] = "<unused>StormwindPrison",
+		["InstanceType"] = 0
+	},
+	["36"] = {
+		["Name"] = "Deadmines",
+		["InstanceType"] = 1
+	},
+	["37"] = {
+		["Name"] = "Azshara Crater",
+		["InstanceType"] = 0
+	},
+	["42"] = {
+		["Name"] = "Collin's Test",
+		["InstanceType"] = 0
+	},
+	["43"] = {
+		["Name"] = "Wailing Caverns",
+		["InstanceType"] = 1
+	},
+	["44"] = {
+		["Name"] = "<unused> Monastery",
+		["InstanceType"] = 1
+	},
+	["47"] = {
+		["Name"] = "Razorfen Kraul",
+		["InstanceType"] = 1
+	},
+	["48"] = {
+		["Name"] = "Blackfathom Deeps",
+		["InstanceType"] = 1
+	},
+	["70"] = {
+		["Name"] = "Uldaman",
+		["InstanceType"] = 1
+	},
+	["90"] = {
+		["Name"] = "Gnomeregan",
+		["InstanceType"] = 1
+	},
+	["109"] = {
+		["Name"] = "Sunken Temple",
+		["InstanceType"] = 1
+	},
+	["129"] = {
+		["Name"] = "Razorfen Downs",
+		["InstanceType"] = 1
+	},
+	["169"] = {
+		["Name"] = "Emerald Dream",
+		["InstanceType"] = 2
+	},
+	["189"] = {
+		["Name"] = "Scarlet Monastery",
+		["InstanceType"] = 1
+	},
+	["209"] = {
+		["Name"] = "Zul'Farrak",
+		["InstanceType"] = 1
+	},
+	["229"] = {
+		["Name"] = "Blackrock Spire",
+		["InstanceType"] = 1
+	},
+	["230"] = {
+		["Name"] = "Blackrock Depths",
+		["InstanceType"] = 1
+	},
+	["249"] = {
+		["Name"] = "Onyxia's Lair",
+		["InstanceType"] = 2
+	},
+	["269"] = {
+		["Name"] = "Caverns of Time",
+		["InstanceType"] = 1
+	},
+	["289"] = {
+		["Name"] = "Scholomance",
+		["InstanceType"] = 1
+	},
+	["309"] = {
+		["Name"] = "Zul'Gurub",
+		["InstanceType"] = 2
+	},
+	["329"] = {
+		["Name"] = "Stratholme",
+		["InstanceType"] = 1
+	},
+	["349"] = {
+		["Name"] = "Maraudon",
+		["InstanceType"] = 1
+	},
+	["369"] = {
+		["Name"] = "Deeprun Tram",
+		["InstanceType"] = 0
+	},
+	["389"] = {
+		["Name"] = "Ragefire Chasm",
+		["InstanceType"] = 1
+	},
+	["409"] = {
+		["Name"] = "Molten Core",
+		["InstanceType"] = 2
+	},
+	["429"] = {
+		["Name"] = "Dire Maul",
+		["InstanceType"] = 1
+	},
+	["449"] = {
+		["Name"] = "Alliance PVP Barracks",
+		["InstanceType"] = 0
+	},
+	["450"] = {
+		["Name"] = "Horde PVP Barracks",
+		["InstanceType"] = 0
+	},
+	["451"] = {
+		["Name"] = "Development Land",
+		["InstanceType"] = 0
+	},
+	["469"] = {
+		["Name"] = "Blackwing Lair",
+		["InstanceType"] = 2
+	},
+	["489"] = {
+		["Name"] = "Warsong Gulch",
+		["InstanceType"] = 3
+	},
+	["509"] = {
+		["Name"] = "Ruins of Ahn'Qiraj",
+		["InstanceType"] = 2
+	},
+	["529"] = {
+		["Name"] = "Arathi Basin",
+		["InstanceType"] = 3
+	},
+	["531"] = {
+		["Name"] = "Ahn'Qiraj Temple",
+		["InstanceType"] = 2
+	},
+	["533"] = {
+		["Name"] = "Naxxramas",
+		["InstanceType"] = 2
+	}
+};
